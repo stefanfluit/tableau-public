b5cb767cfb3fc804c20e0ac8a2318da0fcee2f8a
Stefan Fluit <fluit@datlinq.com>
2019-09-04 09:17:47 +0200

    Changes to systemd units and backupscript

--- files/backup-script.sh
+++ files/backup-script.sh
@@ -1,35 +1,44 @@
 #!/usr/bin/env bash
 
 # Author: Stefan Fluit
-# Target: To backup TSM daily
-# I wrote a seperate script for this because it is a stripped version of my production script, which includes backup to
-# our backup servers. Uncomment row 32 if you dont want the server to back it up to AWS.
+# Target: To backup TSM daily.
 
+# Variables
+declare TSM_BIN="/opt/tableau/tableau_server/packages/customer-bin"
 declare DefLocation="/var/opt/tableau/tableau_server/data/tabsvc/files/backups/"
 declare DateStamp=$(date +"%m_%d_%Y")
+declare Files01User="root"
+declare Files01IP="172.17.2.10"
+declare BackupLocation="/mnt/bigstorage2/tableau_linux"
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:${TSM_BIN}
 
 # Functions
 Configure_Path() {
   source /etc/profile.d/tableau_server.sh
 }
 
-# Backup file will automatically be written to '/var/opt/tableau/tableau_server/data/tabsvc/files/backups/backup.tsbak'
+# Backup file will automatically be written to '/var/opt/tableau/tableau_server/data/tabsvc/files/backups/backup<DateStamp>.tsbak'
 backup_tsm() {
-  printf "Deleting old local files..\n"
-  rm -rf "${DefLocation}"/*.tsbak
-  Configure_Path
-  tsm maintenance backup --file backup_"${DateStamp}".tsbak
+    printf "Deleting old local files..\n"
+    Configure_Path
+    tsm maintenance backup --file backup_"${DateStamp}".tsbak
 }
 
-push_to_aws() {
-	source /var/scripts/push_to_aws.sh "${DefLocation}" backup_"${DateStamp}".tsbak
+count_backups() {
+  local -i count=$("ls -al ${BackupLocation} | wc -l")
+    if [[ "${count}" -gt 15  ]]
+    then
+      "ls -A1t ${BackupLocation} | tail -n +11 | xargs rm"
+    else
+      printf "Retention not exceeded, yet."
+    fi
 }
 
 main() {
-  printf "Starting TSM backup..\n"
-  backup_tsm
-  printf "Done with TSM backup..\n"
-  push_to_aws
+    printf "Starting TSM backup script..\n" && backup_tsm
+    printf "Done with TSM backup..\n"
+    printf "Starting cleanup of old backups..\n"
+    count_backups && printf "\nDone cleaning. Bye!\n"
 }
 
 main
